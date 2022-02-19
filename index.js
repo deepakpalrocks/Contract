1bf57245fc4f9b6cae98b16476a0c18bfcd56f50
Deepak Pal <deepakpalrocks@gmail.com>
2022-02-19 19:54:38 +0530

    aboutUs page added

--- index.js
+++ index.js
@@ -6,9 +6,14 @@
         let nav_height=document.getElementById("MyHeader").offsetHeight;
         if ((scroll > vh-nav_height)) {
             document.getElementById("MyHeader").style.backgroundColor='#1ABC9C';
+            document.getElementById("MyHeader").style.boxShadow='0px 5px 7px 5px rgba(0, 0, 0, 0.233)';
+
         }
         else 
-        document.getElementById("MyHeader").style.backgroundColor='transparent';
+        {
+            document.getElementById("MyHeader").style.backgroundColor='transparent';
+            document.getElementById("MyHeader").style.boxShadow='0px 5px 7px 5px rgba(0, 0, 0, 0)';
+        }
         if((scroll>400)&&vw<=700)
         {
             document.getElementById("nav-title").style.display='block';
