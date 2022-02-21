924dd800fa2ddbc838851438b7a2850607acbefd
Deepak Pal <deepakpalrocks@gmail.com>
2022-02-21 12:46:51 +0530

    nav changes

--- index.js
+++ index.js
@@ -5,12 +5,13 @@
         let vh=window.innerHeight;
         let nav_height=document.getElementById("MyHeader").offsetHeight;
         if ((scroll > vh-nav_height)) {
-            document.getElementById("MyHeader").style.backgroundColor='#1ABC9C';
+            document.getElementById("MyHeader").style.backgroundImage='linear-gradient(45deg,rgba(0, 0, 0, 0.849) 50%,#333333ff 50%)';
             document.getElementById("MyHeader").style.boxShadow='0px 5px 7px 5px rgba(0, 0, 0, 0.233)';
 
         }
         else 
         {
+            document.getElementById("MyHeader").style.backgroundImage='linear-gradient(45deg,rgba(0, 0, 0, 0) ,transparent )';
             document.getElementById("MyHeader").style.backgroundColor='transparent';
             document.getElementById("MyHeader").style.boxShadow='0px 5px 7px 5px rgba(0, 0, 0, 0)';
         }
