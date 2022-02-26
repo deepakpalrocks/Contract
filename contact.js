92397181fe5f24179b9417f2c0ab422f376391d6
Deepak Pal <deepakpalrocks@gmail.com>
2022-02-26 22:06:43 +0530

    updates as per demand

--- contact.js
+++ contact.js
@@ -2,6 +2,10 @@ function copynumber(){
     navigator.clipboard.writeText("+919557682562");
     document.getElementById('numbertooltip').innerHTML="Copied Successfully!";
 }
+function copynumber2(){
+    navigator.clipboard.writeText("+919999874553");
+    document.getElementById('numbertooltip2').innerHTML="Copied Successfully!";
+}
 function copymail(){
     navigator.clipboard.writeText("urbandriveevllp@gmail.com");
     document.getElementById('mailtooltip').innerHTML="Copied Successfully!";
@@ -12,11 +16,14 @@ function copyaddress(){
 }
 
 
+function numberover2(){
+    document.getElementById('numbertooltip2').innerHTML="Click to make a call";
+}
 function numberover(){
-    document.getElementById('numbertooltip').innerHTML="Click to copy";
+    document.getElementById('numbertooltip').innerHTML="Click to make a call";
 }
 function mailover(){
-    document.getElementById('mailtooltip').innerHTML="Click to copy";
+    document.getElementById('mailtooltip').innerHTML="Click to open Mail app";
 }
 function addressover(){
     document.getElementById('addresstooltip').innerHTML="Click to copy";
