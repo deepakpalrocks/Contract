3ff9c749ebe0150737bafe60d0fba52d1276079a
Deepak Pal <deepakpalrocks@gmail.com>
2022-03-01 18:49:41 +0530

    improvements

--- index.js
+++ index.js
@@ -7,6 +7,8 @@
         if ((scroll > vh-nav_height)) {
             document.getElementById("MyHeader").style.backgroundImage='linear-gradient(45deg,rgba(0, 0, 0, 0.849) 50%,#333333ff 50%)';
             document.getElementById("MyHeader").style.boxShadow='0px 5px 7px 5px rgba(0, 0, 0, 0.233)';
+            document.getElementById("navicon").style.transform='translateY(0)';
+
 
         }
         else 
@@ -14,8 +16,9 @@
             document.getElementById("MyHeader").style.backgroundImage='linear-gradient(45deg,rgba(0, 0, 0, 0) ,transparent )';
             document.getElementById("MyHeader").style.backgroundColor='transparent';
             document.getElementById("MyHeader").style.boxShadow='0px 5px 7px 5px rgba(0, 0, 0, 0)';
+            document.getElementById("navicon").style.transform='translateY(-150%)';
         }
-        if((scroll>400)&&vw<=700)
+        if((scroll>400)&&vw<=1100)
         {
             document.getElementById("nav-title").style.display='block';
         }
